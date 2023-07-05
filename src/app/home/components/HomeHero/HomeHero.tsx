7e42c604241c8d419f7b4b4a8b9317dfa14dfd2b
Kaluã Bentes <kaluanbentes@gmail.com>
2023-07-05 03:46:30 -0400

    refactor: return info

--- src/app/home/components/HomeHero/HomeHero.tsx
+++ src/app/home/components/HomeHero/HomeHero.tsx
@@ -5,7 +5,7 @@ import Hero from "@/design-system/components/Hero"
 const HomeHero = () => (
   <Hero
     headline="Kaluã Bentes"
-    paragraph="Desenvolvedor Frontend JAMStack, estudante de UI/UX e entusiasta de jogos HTML5."
+    paragraph="Desenvolvedor Frontend JAMStack, estudante de UI/UX e entusiasta de jogos HTML5. Minha missão é entregar telas de alta fidelidade, eficazes, acessíveis, performáticas e com um código escalável."
     buttonText="Vamos conversar"
     imageSrc="/kalua-bentes.png"
     imageAlt="Kaluã Bentes Avatar"
